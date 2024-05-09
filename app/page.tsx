a482855b94a73b3248983a9f9c758c0685e6170c
nftmoondogs <nftmoondogs@gmail.com>
2024-05-09 17:53:05 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import Image from "next/image";
+import Wrapper from "@/components/wrapper";
+import { Button } from "@/components/ui/button";
+
+import robot from "@/public/images/robot.png";
+
+export default function Home() {
+  return (
+    <section className="flex flex-col lg:flex-row">
+      <section className="flex h-screen w-full flex-col justify-between p-9 lg:h-auto">
+        <Wrapper>
+          <div className="mx-auto flex max-w-sm flex-col justify-between">
+            <span
+              className={`-mt-14 inline-block text-[64px] font-bold text-black dark:text-green-600`}
+            >
+              $0.5
+            </span>
+            <p className="pb-6 font-medium text-white">
+            Dive into a trust-based experiment designed to ensure fair distribution, faster bonding curve completion time  and eliminate whale domination. By participating with just 0.5 SOL or lower, you not only build trust within the project but this is the first step of a three-step wider strategy where those who stick to the rules will be rewarded. You may have noticed that most of the projects that fail have a weak distribution, we intend to combat this. Good luck and stay faithful.
+            </p>
+         
+
+            <div className="">
+              <Button size="xl" className="w-full font-bold" variant="brand">
+                <a
+                  href="https://pump.fun/board"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="pb-1 dark:text-zinc-800 text-zinc-100"
+                >
+                  Buy From Pump.fun
+                </a>{" "}
+              </Button>
+            </div>
+            <div className="pt-2">
+              <Button size="xl" className="w-full font-bold" variant="brand">
+                <a
+                  href="https://pump.fun/board"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="pb-1 dark:text-zinc-800 text-zinc-100"
+                >
+                  Telegram
+                </a>{" "}
+              </Button>
+            </div>
+            <div className="pt-2">
+              <Button size="xl" className="w-full font-bold" variant="brand">
+                <a
+                  href="https://pump.fun/board"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="pb-1 dark:text-zinc-800 text-zinc-100"
+                >
+                  Twitter
+                </a>{" "}
+              </Button>
+            </div>
+          </div>
+        </Wrapper>
+      </section>
+
+      {/* second half */}
+
+      <section className="hidden lg:flex h-screen w-full flex-col justify-center items-center bg-[#f1efe7] p-9">
+        <Image src={robot} alt="Man sitting in wheelchair" />
+      </section>
+    </section>
+  );
+}
---
+++ components/wrapper.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+import ThemeToggle from "./theme-toggle";
+import { Button } from "./ui/button";
+import Link from "next/link";
+
+const SECTION_DATA = [
+  { label: 1, href: "/", isFirst: true, isLast: false },
+  { label: 2, href: "/page-2", isFirst: true, isLast: false },
+  { label: 3, href: "/page-3", isFirst: false, isLast: true },
+];
+
+export default function Wrapper({ children }: { children: React.ReactNode }) {
+  const [currentIndex, setCurrentIndex] = useState(1);
+  const [nextPage, setNextPage] = useState<undefined | string>("/");
+  const [previousPage, setPreviousPage] = useState<undefined | string>("/");
+  const pathname = usePathname();
+  const activeSection = SECTION_DATA.find(
+    (section) => section.href === pathname
+  );
+
+  useEffect(() => {
+    if (activeSection?.isFirst || activeSection?.isLast == false) {
+      setNextPage(`page-${currentIndex + activeSection.label}`);
+    } else {
+      setPreviousPage(`page-${Math.abs(currentIndex - activeSection?.label!)}`);
+    }
+  }, [
+    activeSection?.isFirst,
+    activeSection?.isLast,
+    activeSection?.label,
+    currentIndex,
+  ]);
+
+  return (
+    <>
+      <div className="flex w-full items-center justify-between">
+        <Link
+          href="/"
+          className={`flex items-center text-2xl font-bold dark:text-green-600`}
+        >
+          $0.5{" "}
+          <span
+            className={`text-sm font-bold group ml-2 inline-block rounded-3xl bg-[#00FF00] px-3 text-black`}
+          >
+            <span className="">Token</span>
+          </span>
+        </Link>
+
+  
+      </div>
+      {children}
+      <div className="flex w-full items-center justify-between">
+
+
+      </div>
+    </>
+  );
+}
